cfdccdba5f2299cb5f2eb23274b79ab7b99730c6
harsheeen <harsheenkaur023@gmail.com>
2026-02-18 10:54:08 +0530

    hello

---
+++ OopsUCthree.java
@@ -0,0 +1,18 @@
+/** 
+* OOPSBANNERAPP UC3 - OOPS BANNER Display Application with join function
+* @author Developer
+* @version 2.0
+**/
+class OopsUCthree{
+	public static void main(String[] args){
+		System.out.println(String.join(" " ,"   ***   "  ,  "   ***   "  ,  " ******   " ,  "  ***** "));
+		System.out.println(String.join(" " ," **   **"  ,  "  **   **"  ,  "  **   **"  ,   " **"));
+		System.out.println(String.join(" " ,"**     **"  ,  "**     **"  ,  " **    **"  ,  "**"));
+		System.out.println(String.join(" " ,"**     **"  ,  "**     **"  , "  **   **"  ,    " **"));
+		System.out.println(String.join(" " ,"**     **"  ,  "**     **"  ,  " ****** "  ,  "   ****"));
+		System.out.println(String.join(" " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "      **"));
+		System.out.println(String.join(" " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "       **"));
+		System.out.println(String.join(" " ," **   **"  ,  "  **   **"  ,  "  **     "  ,  "      **"));
+		System.out.println(String.join(" " ,"   ***    " ,"  ***  "  ,"  **  "  ,   "    ***** "));
+	}
+}
